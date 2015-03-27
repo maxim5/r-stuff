f2a43ed10e58a3cf88c2e8f7b1d3d0d51ab4c8ee
maximp <maximp@localhost>
2015-03-27 18:28:18 +0100

    more on exdata-012 tasks

--- exdata-012/project-2/plot4.R
+++ exdata-012/project-2/plot4.R
@@ -4,18 +4,18 @@ source("common.R")
 
 sectors = unique(scc_mapping$EI.Sector)
 coal_sectors = sectors[grep("Coal", sectors)]
-coal_mapping = scc_mapping %>% filter(EI.Sector %in% u[grep("Coal", u)])
+coal_mapping = scc_mapping %>% filter(EI.Sector %in% coal_sectors)
 
 coal_summary = inventory %>%
-  filter(SCC %in% coal_mapping$SCC) %>%
-  group_by(year) %>%
-  summarise(total_emission=sum(Emissions) / 1000)
+               filter(SCC %in% coal_mapping$SCC) %>%
+               group_by(year) %>%
+               summarise(total_emission=sum(Emissions) / 1000)
 
 ggplot(coal_summary,
        aes(x=as.factor(year), y=total_emission)) + 
   geom_bar(stat="identity",
            fill="green", colour="black") + 
-  labs(title="Total emissions from PM2.5 from coal combustion-related sources",
+  labs(title=expression("Total emissions from PM"[2.5]*" from coal combustion-related sources"),
        x="", y="Total emission (in 1000 tones)")
 dev.copy(png, file="plot4.png")
 dev.off()
