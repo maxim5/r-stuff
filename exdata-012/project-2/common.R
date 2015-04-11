56c7d29194f66e15fe326248914f43002cb4c077
Maxim Podkolzine <2580604+maxim5@users.noreply.github.com>
2015-04-11 17:27:51 +0200

    exdata project 2

--- exdata-012/project-2/common.R
+++ exdata-012/project-2/common.R
@@ -1,10 +1,17 @@
 library(dplyr)
 library(magrittr)
 library(ggplot2)
+library(grid)
 
-par(mar=c(5, 4, 4, 4), ps=12)
+#inventory = readRDS("summarySCC_PM25.rds") %>%
+#            subset(select=-c(Pollutant)) %>%    # holds only "PM25-PRI"
+#            tbl_df()
+#scc_mapping = readRDS("Source_Classification_Code.rds") %>% tbl_df()
 
-inventory = readRDS("summarySCC_PM25.rds") %>%
-            subset(select=-c(Pollutant)) %>%    # holds only "PM25-PRI"
-            tbl_df()
-scc_mapping = readRDS("Source_Classification_Code.rds") %>% tbl_df()
+BAR_COLOR = "#00BFC4"
+BAR_BORDER_COLOR = "#AAAAAA"
+PLOT_LINE_COLOR = "darkgreen"
+OPAQUE = rgb(0, 0, 0, 0)
+POINT_FILL_COLOR = "white"
+
+par(mar=c(5, 7, 4, 4), mfrow=c(1, 1), mgp=c(4, 1.5, 1), ps=12)
--- exdata-012/project-2/plot6.R
+++ exdata-012/project-2/plot6.R
@@ -7,12 +7,54 @@ motor_mapping = scc_mapping %>% filter(Short.Name %in% motor_names)
 motor_summary_baltimore = inventory %>%
                           filter(SCC %in% motor_mapping$SCC, fips == "24510") %>%
                           group_by(year) %>%
-                          summarise(total_emission=sum(Emissions))
+                          summarise(total_emission=sum(Emissions)) %>%
+                          mutate(location="Baltimore City") %>%
+                          mutate(diff=c(0, diff(total_emission)))
 
 motor_summary_la = inventory %>%
                    filter(SCC %in% motor_mapping$SCC, fips == "06037") %>%
                    group_by(year) %>%
-                   summarise(total_emission=sum(Emissions))
+                   summarise(total_emission=sum(Emissions)) %>%
+                   mutate(location="LA County") %>%
+                   mutate(diff=c(0, diff(total_emission)))
 
-print(motor_summary_baltimore)
-print(motor_summary_la)
+combined_summary = rbind(motor_summary_baltimore, motor_summary_la) %>% tbl_df()
+
+TITLE = expression(atop("Comparison of emissions from PM"[2.5]*" from motor vehicles in ",
+                        "Baltimore City and Los Angeles County"))
+XLAB = ""
+YLAB = "Total emission (in tones)"
+THEME = theme(plot.margin=unit(c(1,1,1,1), "cm"),
+              plot.title=element_text(vjust=2),
+              axis.title.y=element_text(angle=90, vjust=2))
+
+# View as barplots
+# Shows the absolute values
+ggplot(combined_summary,
+       aes(x=as.factor(location), y=total_emission, fill=as.factor(location))) + 
+  geom_bar(stat="identity") + 
+  facet_grid(. ~ year) +
+  labs(title=TITLE, x=XLAB, y=YLAB) +
+  scale_fill_discrete(name="Location") +
+  THEME +
+  theme(axis.text.x=element_blank(),
+        axis.title.x=element_blank(),
+        axis.ticks.x=element_blank())
+ggsave("plot6.png")
+
+# Alternative view - several lines on a plot
+# Shows the changes over time
+TITLE = expression(atop("Changes in emissions from PM"[2.5]*" from motor vehicles in ",
+                        "Baltimore City and Los Angeles County"))
+XLAB = ""
+YLAB = "Change of total emission (in tones)"
+
+ggplot(combined_summary,
+       aes(x=year, y=diff, group=location, color=location)) +
+  geom_line() +
+  geom_point(size=4, shape=21, fill=POINT_FILL_COLOR) +
+  scale_x_continuous(breaks=seq(min(combined_summary$year), max(combined_summary$year), by=3)) +
+  labs(title=TITLE, x=XLAB, y=YLAB) +
+  scale_color_discrete(name="Location") +
+  THEME
+ggsave("plot6-alt.png")
