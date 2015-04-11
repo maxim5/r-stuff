acd0b7a16ea8491c731fab74c544d97502fd64db
Maxim Podkolzine <2580604+maxim5@users.noreply.github.com>
2015-04-11 17:42:28 +0200

    minor

--- exdata-012/project-2/common.R
+++ exdata-012/project-2/common.R
@@ -3,10 +3,10 @@ library(magrittr)
 library(ggplot2)
 library(grid)
 
-#inventory = readRDS("summarySCC_PM25.rds") %>%
-#            subset(select=-c(Pollutant)) %>%    # holds only "PM25-PRI"
-#            tbl_df()
-#scc_mapping = readRDS("Source_Classification_Code.rds") %>% tbl_df()
+inventory = readRDS("summarySCC_PM25.rds") %>%
+            subset(select=-c(Pollutant)) %>%    # holds only "PM25-PRI"
+            tbl_df()
+scc_mapping = readRDS("Source_Classification_Code.rds") %>% tbl_df()
 
 BAR_COLOR = "#00BFC4"
 BAR_BORDER_COLOR = "#AAAAAA"
