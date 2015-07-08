f6a96bf99760f1cfa2d2ef9b75abea8e76c56c24
maximp <maximp@localhost>
2015-07-08 12:47:53 +0200

    lab 6

---
+++ compfinance-009/lab-6_Constant_Expected_Return_Model/main.R
@@ -0,0 +1,82 @@
+library("boot")
+library("zoo")
+library("tseries")
+
+VBLTX_prices <- get.hist.quote(instrument="vbltx", start="2005-09-01", end="2010-09-30", quote="AdjClose",provider="yahoo", origin="1970-01-01",compression="m", retclass="zoo", quiet = TRUE)
+FMAGX_prices <- get.hist.quote(instrument="fmagx", start="2005-09-01", end="2010-09-30", quote="AdjClose",provider="yahoo", origin="1970-01-01",compression="m", retclass="zoo", quiet = TRUE)
+SBUX_prices <- get.hist.quote(instrument="sbux", start="2005-09-01",end="2010-09-30", quote="AdjClose",provider="yahoo", origin="1970-01-01",compression="m", retclass="zoo", quiet = TRUE)
+
+# Change the class of the time index to yearmon, which is appropriate for monthly data.
+# index() and as.yearmon() are functions in the zoo package 
+index(VBLTX_prices) <- as.yearmon(index(VBLTX_prices))
+index(FMAGX_prices) <- as.yearmon(index(FMAGX_prices))
+index(SBUX_prices) <- as.yearmon(index(SBUX_prices))
+
+# Create merged price data
+all_prices <- merge(VBLTX_prices, FMAGX_prices, SBUX_prices)
+# Rename columns
+colnames(all_prices) <- c("VBLTX", "FMAGX", "SBUX")
+
+# Calculate cc returns as difference in log prices
+all_returns <- diff(log(all_prices))
+
+# Create matrix with returns
+return_matrix <- coredata(all_returns)
+
+########################################################################################
+
+# Number of observations
+n_obs <- nrow(return_matrix)
+
+# Estimates of sigma2hat
+sigma2hat_vals <- diag(var(return_matrix))
+
+# Standard Error of sigma2hat
+se_sigma2hat <- sigma2hat_vals / sqrt(n_obs / 2)
+
+########################################################################################
+
+# Calculate the correlation matrix
+cor_matrix <- cor(return_matrix)
+
+# Get the lower triangular part of that 'cor_matrix'
+rhohat_vals <- c(cor_matrix[2,][1], cor_matrix[3,][1], cor_matrix[3,][2])
+
+# Set the names
+names(rhohat_vals) <- c("VBLTX,FMAGX","VBLTX,SBUX","FMAGX,SBUX")
+
+# Compute the estimated standard errors for correlation
+se_rhohat <- (1 - rhohat_vals^2) / sqrt(nrow(return_matrix))
+
+########################################################################################
+
+vbltx = all_returns$VBLTX
+t.test(vbltx)
+
+########################################################################################
+
+# Test the correlation between VBLTX,FMAGX
+cor.test(all_returns$VBLTX, all_returns$FMAGX)
+
+########################################################################################
+
+# Test the normality of the returns of VBLTX
+jarque.bera.test(all_returns$VBLTX)
+
+########################################################################################
+
+# Function for bootstrapping sample mean: 
+mean_boot <- function(x, idx) {
+  ans <- mean(x[idx])
+  ans 
+} 
+# Construct VBLTX_mean_boot:
+VBLTX_mean_boot <- boot(return_matrix[,"VBLTX"], statistic = mean_boot, R=999)
+# Print the class of VBLTX_mean_boot
+class(VBLTX_mean_boot)
+
+# Print VBLTX_mean_boot
+VBLTX_mean_boot
+
+# Plot bootstrap distribution and qq-plot against normal
+plot(VBLTX_mean_boot)
